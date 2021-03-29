3a89bf3f725085ff709fc70c7b83d4fb7255e87e
aprin418 <aprin418@gmail.com>
2021-03-29 11:54:21 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,9 +26,9 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-      this.toggleModal();
-      this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
-  }
+        this.toggleModal();
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }
 
     render() {
         return (
@@ -116,7 +116,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -133,7 +133,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         );
                     })
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -176,10 +176,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                    comments={props.comments}
-                    addComment={props.addComment}
-                    campsiteId={props.campsite.id} />
+                    <RenderComments
+                        comments={props.comments}
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id}
+                    />
 
                 </div>
             </div>
